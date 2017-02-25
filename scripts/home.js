28e0bcd7e448722f6fbb315fb414647ccc01c693
Tania Sorotiuk <tsorotiuk@gmail.com>
2017-02-26 01:02:26 +0200

    task_08: service buttons hover (does not work)

--- scripts/home.js
+++ scripts/home.js
@@ -3,3 +3,15 @@
  */
 
 console.log('start js logic');
+
+var btnServ = document.getElementsByClassName('_btn-service');
+var picServ = document.getElementsByClassName('_pic-service');
+console.log(btnServ);
+console.log(picServ);
+
+//var i=0;
+//for (var i=0; i<btnServ.length; i++){
+//    btnServ[i].onclick = function() {
+//        picServ.className = 'bg-second' ;
+//    };
+//}
